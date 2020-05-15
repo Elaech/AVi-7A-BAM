dd62baf7fd128108aa2ef522b01bda58768258f0
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2020-05-15 21:42:00 +0300

    Patched AUTH

--- AVIAUTH/app/controllers/updateaccount.php
+++ AVIAUTH/app/controllers/updateaccount.php
@@ -13,7 +13,7 @@ class UpdateAccount extends Controller {
             $token = $data['token'];
             $payload = $this->verifyToken($token,$ip);
             if($payload == null){
-                $this->set_response(400, ['status'=>false,'ip_error' => "Invalid IP"]);
+                $this->set_response(400, ['status'=>false,'ip_error' => "Invalid Token"]);
                 return $this->response;
             }
             $id = $payload['id'];
