a2c87a704be8929985b1e6a39bca2b088c3c6d35
andraionita <44443291+andraionita@users.noreply.github.com>
2020-06-09 21:00:27 +0300

    Update test.c

--- test.c
+++ test.c
@@ -1,10 +1,21 @@
+int vr_sa_ma_omor_bogdan(char a){
+    return "puti";
+}
 int vr_sa_ma_omor(char a){
+    return "puti";
+}
+int nogdan(char a){
     return "puti";
 }
 int tell_me_why(int number){
 	return number;
 }
-
+int vrau(char a){
+    return "puti";
+}
 int main(){
 
+}
+int puti(char a){
+    return "puti";
 }
