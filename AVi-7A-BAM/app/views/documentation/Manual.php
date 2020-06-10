8580038b6631b6fde9eab269f20a5c8ed78bc3fa
Bogdan405 <Bogdan405@outlook.com>
2020-06-10 14:24:04 +0300

    Create Manual.php

---
+++ AVi-7A-BAM/app/views/documentation/Manual.php
@@ -0,0 +1,41 @@
+<head>
+    <meta charset="UTF-8">
+	<link rel="icon" href="http://localhost/AVi-7A-BAM/public/Styles/logo-icon.png" type="image/gif">
+    <title>AVI MANUAL</title>
+</head>
+
+<style>
+    body{
+        background-color: white;
+        padding-left: 20%;
+        padding-right: 20%;
+        padding-top: 3rem;
+    }
+	a{
+		text-decoration:none;
+	}
+    p{
+        padding-left: 0rem;
+    }
+	#toc_container {
+
+	margin-bottom: 1em;
+	padding: 20px;
+	width: auto;
+	}
+	ol{
+		text-decoration:none;
+		list-style: none;
+	}
+
+
+</style>
+
+<body>
+    <article>
+        <header>
+            <h1>MANUAL AVI</h1>
+        </header>
+		
+	</article>
+</body>
