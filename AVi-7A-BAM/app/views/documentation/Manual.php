f03ac0a31db6d092e9148ee85fc50d580232af1f
Bogdan405 <Bogdan405@outlook.com>
2020-06-11 16:19:59 +0300

    Update Manual.php

--- AVi-7A-BAM/app/views/documentation/Manual.php
+++ AVi-7A-BAM/app/views/documentation/Manual.php
@@ -49,77 +49,81 @@
 		
 		<section>
 			<h2>Navigare</h2>
-			<p></p>
+			<p>Pentru a naviga pe paginile site-uluise foloseste bara de navigare din stanga, aceasta se extinde cand se da hover cu mause-ul, astfel 
+			fiind fizibile numele paginilor care pot fi accesate. Accesarea se face prin click pe butonul corespunzator paginii dorite.</p>
 			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
 			<img src="./Manual/HomeNav.PNG">
 		</section>
 		
 		<section>
 			<h2>Contact</h2>
-			<p></p>
+			<p>Pagina de contact contine informatii generale despre noi, si date de contact.</p>
 			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
 			<img src="./Manual/Contact.PNG">
 		</section>
 		
 		<section>
 			<h2>Creere cont</h2>
-			<p></p>
+			<p>Pentru a putea accesa statistici trebuie creeat un cont de utilizator, acest lucru se face din pagina creere cont, completand campurile 
+			formularului cu informatii corespunzatoare si apasand submit.</p>
 			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
 			<img src="./Manual/CreateAcc.PNG">
 		</section>
 		
 		<section>
 			<h2>Logare</h2>
-			<p></p>
+			<p>Odata creeat un cont, se acceseaza pagina de logare, unde se completeaza campurle nume si parola cu datele completate initial la creeare cont, si 
+			sa apasa submit.</p>
 			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
 			<img src="./Manual/SignIn.PNG">
 		</section>
 		
 		<section>
 			<h2>Statistici</h2>
-			<p></p>
+			<p>Pentru un utilizator logat, va fi accesibila pagina de Statistici, prin intermediul cereia se pot vedea date despre accidente si varii statistici cu acele date.</p>
 			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
 			<img src="./Manual/StatisticsEmpty.PNG">
 		</section>
 		
 		<section>
 			<h2>Alegere informatii accidente</h2>
-			<p></p>
+			<p>Se pot alege si filtra datele afisate si folosite la generarea statisticilor prin alegerea campurilor afisate, si a limitarii valorilor acestora.</p>
 			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
 			<img src="./Manual/StatisticsMenu.PNG">
 		</section>
 		
 		<section>
 			<h2>Alegere format grafic</h2>
-			<p></p>
+			<p>Graficul generat cu informatiile selectate poate fi de mai multe tipuri, tipul se poate selecta in meniul Format Result.</p>
 			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
 			<img src="./Manual/StatisticsFormat.PNG">
 		</section>
 		
 		<section>
 			<h2>Alegere elemente grafic</h2>
-			<p></p>
+			<p>Infomratiile interpretate de grafic pot fi selectate in meniul Area Field.</p>
 			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
 			<img src="./Manual/StatisticsField.PNG">
 		</section>
 		
 		<section>
 			<h2>Format descarcare</h2>
-			<p></p>
+			<p>Graficul statistic generat poate fi descarcat in mai multe formate, acestea se pot schimba in meniul Download As.</p>
 			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
 			<img src="./Manual/StatisticsDownload.PNG">
 		</section>
 		
 		<section>
 			<h2>Tabel afisat dupa search</h2>
-			<p></p>
+			<p>Dupa selectia campurilor si graficului dorit, la apasare butonului search vor aparea datele tabelate, iar prin apasare butonului View Data, 
+			se va afisa graficul. Apasarea butonului Download va genera un prompt menu pentru descarcare.</p>
 			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
 			<img src="./Manual/AfterSearchTable.PNG">
 		</section>
 		
 		<section>
 			<h2>Paginare Tabel</h2>
-			<p></p>
+			<p>Pentru accesibilitate tabelul este organiat pe mai multe pagini, acestea sa pot accesa prin butoanele de paginare de sub tabel.</p>
 			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
 			<img src="./Manual/AfterSearchPages.PNG">
 		</section>
