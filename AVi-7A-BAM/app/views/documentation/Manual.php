09af4c6530875076a89328c4b97127973c52b052
Bogdan405 <Bogdan405@outlook.com>
2020-06-11 16:34:48 +0300

    Update Manual.php

--- AVi-7A-BAM/app/views/documentation/Manual.php
+++ AVi-7A-BAM/app/views/documentation/Manual.php
@@ -43,7 +43,7 @@
 		<section>
 			<h2>Home</h2>
 			<p>Prima pagina care va aparea la accesarea site-ului este pagina Home, de mai jos, aceasta contine informatii generale despre site.</p>
-			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
+			<!-- <img src="http://localhost/AVi-7A-BAM/public/Styles/Manual/Home.PNG"> -->
 			<img src="./Manual/Home.PNG">
 		</section>
 		
@@ -51,14 +51,14 @@
 			<h2>Navigare</h2>
 			<p>Pentru a naviga pe paginile site-uluise foloseste bara de navigare din stanga, aceasta se extinde cand se da hover cu mause-ul, astfel 
 			fiind fizibile numele paginilor care pot fi accesate. Accesarea se face prin click pe butonul corespunzator paginii dorite.</p>
-			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
+			<!-- <img src="http://localhost/AVi-7A-BAM/public/Styles/Manual/Home.PNG"> -->
 			<img src="./Manual/HomeNav.PNG">
 		</section>
 		
 		<section>
 			<h2>Contact</h2>
 			<p>Pagina de contact contine informatii generale despre noi, si date de contact.</p>
-			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
+			<!-- <img src="http://localhost/AVi-7A-BAM/public/Styles/Manual/Home.PNG"> -->
 			<img src="./Manual/Contact.PNG">
 		</section>
 		
@@ -66,7 +66,7 @@
 			<h2>Creere cont</h2>
 			<p>Pentru a putea accesa statistici trebuie creeat un cont de utilizator, acest lucru se face din pagina creere cont, completand campurile 
 			formularului cu informatii corespunzatoare si apasand submit.</p>
-			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
+			<!-- <img src="http://localhost/AVi-7A-BAM/public/Styles/Manual/Home.PNG"> -->
 			<img src="./Manual/CreateAcc.PNG">
 		</section>
 		
@@ -74,42 +74,42 @@
 			<h2>Logare</h2>
 			<p>Odata creeat un cont, se acceseaza pagina de logare, unde se completeaza campurle nume si parola cu datele completate initial la creeare cont, si 
 			sa apasa submit.</p>
-			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
+			<!-- <img src="http://localhost/AVi-7A-BAM/public/Styles/Manual/Home.PNG"> -->
 			<img src="./Manual/SignIn.PNG">
 		</section>
 		
 		<section>
 			<h2>Statistici</h2>
 			<p>Pentru un utilizator logat, va fi accesibila pagina de Statistici, prin intermediul cereia se pot vedea date despre accidente si varii statistici cu acele date.</p>
-			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
+			<!-- <img src="http://localhost/AVi-7A-BAM/public/Styles/Manual/Home.PNG"> -->
 			<img src="./Manual/StatisticsEmpty.PNG">
 		</section>
 		
 		<section>
 			<h2>Alegere informatii accidente</h2>
 			<p>Se pot alege si filtra datele afisate si folosite la generarea statisticilor prin alegerea campurilor afisate, si a limitarii valorilor acestora.</p>
-			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
+			<!-- <img src="http://localhost/AVi-7A-BAM/public/Styles/Manual/Home.PNG"> -->
 			<img src="./Manual/StatisticsMenu.PNG">
 		</section>
 		
 		<section>
 			<h2>Alegere format grafic</h2>
 			<p>Graficul generat cu informatiile selectate poate fi de mai multe tipuri, tipul se poate selecta in meniul Format Result.</p>
-			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
+			<!-- <img src="http://localhost/AVi-7A-BAM/public/Styles/Manual/Home.PNG"> -->
 			<img src="./Manual/StatisticsFormat.PNG">
 		</section>
 		
 		<section>
 			<h2>Alegere elemente grafic</h2>
 			<p>Infomratiile interpretate de grafic pot fi selectate in meniul Area Field.</p>
-			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
+			<!-- <img src="http://localhost/AVi-7A-BAM/public/Styles/Manual/Home.PNG"> -->
 			<img src="./Manual/StatisticsField.PNG">
 		</section>
 		
 		<section>
 			<h2>Format descarcare</h2>
 			<p>Graficul statistic generat poate fi descarcat in mai multe formate, acestea se pot schimba in meniul Download As.</p>
-			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
+			<!-- <img src="http://localhost/AVi-7A-BAM/public/Styles/Manual/Home.PNG"> -->
 			<img src="./Manual/StatisticsDownload.PNG">
 		</section>
 		
@@ -117,14 +117,14 @@
 			<h2>Tabel afisat dupa search</h2>
 			<p>Dupa selectia campurilor si graficului dorit, la apasare butonului search vor aparea datele tabelate, iar prin apasare butonului View Data, 
 			se va afisa graficul. Apasarea butonului Download va genera un prompt menu pentru descarcare.</p>
-			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
+			<!-- <img src="http://localhost/AVi-7A-BAM/public/Styles/Manual/Home.PNG"> -->
 			<img src="./Manual/AfterSearchTable.PNG">
 		</section>
 		
 		<section>
 			<h2>Paginare Tabel</h2>
 			<p>Pentru accesibilitate tabelul este organiat pe mai multe pagini, acestea sa pot accesa prin butoanele de paginare de sub tabel.</p>
-			<img src="http://localhost/AVi-7A-BAM/public/Styles/Diagrama%20Arhitecturala.png">
+			<!-- <img src="http://localhost/AVi-7A-BAM/public/Styles/Manual/Home.PNG"> -->
 			<img src="./Manual/AfterSearchPages.PNG">
 		</section>
 		
